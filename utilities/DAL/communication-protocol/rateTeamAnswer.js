931915639b8e4f4ec9cf62e9e0f84b12769f0a2c
Jeffrey <doggycollin1995@gmail.com>
2017-04-09 12:49:40 +0200

    Added missing round id to RateTeamAnswer.

--- utilities/DAL/communication-protocol/rateTeamAnswer.js
+++ utilities/DAL/communication-protocol/rateTeamAnswer.js
@@ -1,9 +1,10 @@
 
 /* Used to rate an answer put by a team (approve or disapprove). */
 class RateTeamAnswer {
-    constructor(gameId, teamId, questionId, accepted) {
+    constructor(gameId, roundId, teamId, questionId, accepted) {
         this.type = "RateTeamAnswer";
         this.gameId = gameId;
+        this.roundId = roundId;
         this.teamId = teamId;
         this.questionId = questionId;
         this.accepted = accepted;
